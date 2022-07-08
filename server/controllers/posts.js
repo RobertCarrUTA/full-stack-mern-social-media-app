63be6d9bca8bb13f8bf776fcad9bee92f07cd829
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-07 21:21:09 -0500

    Added the try-catch for createPost
    Added the try-catch for createPost in controllers/posts.js.

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -6,12 +6,24 @@ export const getPosts = async (req, res) => {
     // Try to retrieve all the posts we currently have in the database (asynchronous action - takes time, use await)
     const postMessages = await PostMessage.find();
 
+    // To see HTTP status codes (.status()):
+    // https://www.restapitutorial.com/httpstatuscodes.html
     res.status(200).json(postMessages);
   } catch {
     res.status(404).json({ message: error.message });
   }
 };
 
-export const createPost = (req, res) => {
-  res.send("Post Creation");
+export const createPost = async (req, res) => {
+  // With posts requests we have access to request.body
+  const post = req.body;
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
