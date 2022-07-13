1f992d1ccb757b6d067acf920571d20e63b1168a
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-13 18:27:36 -0500

    Added a Create a Memory button
    The home page now shows a button that says Create a Memory button but the button has no functionality yet.

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
 
 import useStyles from "./styles";
 
 const Form = () => {
   const classes = useStyles();
 
-  return <h1>Form</h1>;
+  // Handler function
+  const handleSubmit = () => {};
+
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={classes.form}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Creating a Memory</Typography>
+      </form>
+    </Paper>
+  );
 };
 
 export default Form;
