e0844455f7d57ab262f927f768f4ac8cae8eb5de
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-13 18:43:53 -0500

    The button now has a creator text field
    The button that was previously added in the last commit now contains a single text field that is a field for the creator value.

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 
 import useStyles from "./styles";
 
 const Form = () => {
   const classes = useStyles();
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
 
   // Handler function
   const handleSubmit = () => {};
@@ -18,6 +25,19 @@ const Form = () => {
         onSubmit={handleSubmit}
       >
         <Typography variant="h6">Creating a Memory</Typography>
+        {/* This TextField is for who is creating a post */}
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          // This on change allows us to only change the creator value of postData.creator even with multiple TextFields
+          // We have to spread the postData and then set the e.target.value to the creator attribute
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
       </form>
     </Paper>
   );
