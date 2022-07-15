216f42238b7f2f3d6f0c022828435ea9e08db998
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-14 19:24:00 -0500

    Added a loading icon where posts used to be
    Now there is a loading icon where the Post used to repeat on the homepage. I am unsure if it is loading forever because the database is not connected. Just backing up my code.

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,19 +1,35 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
 
 import Post from "./Post/Post";
 import useStyles from "./styles";
 
 const Posts = () => {
   const classes = useStyles();
-  const posts = useSelector((state) => state.posts); // hook
+  const posts = useSelector((state) => state.posts);
 
   return (
-    <>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </>
+    // If posts.length = 0, 0 is true, if posts.length > 0 then it is false
+    //  If it is true, it will show the Grid
+    //  If it is finally, it will show the posts on their own
+    !posts.length ? (
+      <CircularProgress />
+    ) : (
+      // A grid of our posts
+      <Grid
+        className={classes.container}
+        container
+        alignItems="stretch"
+        spacing={3}
+      >
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6}>
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    )
   );
 };
 
